b5e89b9e3bb0ac508c325f7171cfc3b283712e3f
Gabriel Dornas <gabrielbdornas@gmail.com>
2023-07-20 17:02:20 -0300

    2° video - atualizando rota get todos students

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask, request
-from data import students
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    room = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return f"Student('{self.name}', '{self.email}', '{self.room}')"
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
 
 @app.route('/students', methods=['GET'])
 def get():
-    return students
+    students = Student.query.all()
+    return jsonify([student.as_dict() for student in students])
+
 
 @app.route('/students/<int:student_id>', methods=['GET'])
 def get_student(student_id):
