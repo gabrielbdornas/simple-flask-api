8381890e3c7895cfc651cec95038e9553584284e
Gabriel Dornas <gabrielbdornas@gmail.com>
2023-07-20 16:00:06 -0300

    1° video - Add rota get para retornar student por id

--- app.py
+++ app.py
@@ -6,3 +6,10 @@ app = Flask(__name__)
 @app.route('/students', methods=['GET'])
 def get():
     return students
+
+@app.route('/students/<int:student_id>', methods=['GET'])
+def get_student(student_id):
+    return students.get(
+        student_id,
+        {'error': 'Student not found'}
+    )
